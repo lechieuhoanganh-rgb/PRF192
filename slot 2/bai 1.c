9ffd97c7ef3277ca843e037cda4dbdde2aadc00a
lechieuhoanganh-rgb <lechieuhoanganh@gmail.com>
2026-01-09 11:21:01 +0700

    bai 2

--- slot 2/bai 1.c
+++ slot 2/bai 1.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
 int main(){
-	printf("a=10");
+	int a=10; //khai bao bien so nguyen
+	printf("so da khai bao a=%5d",a);//in ra bien da khai bao
+	return 0;//end program
 }
---
+++ slot 2/bai2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	float a=3.1431268;//khai bao bien va gia tri
+	float b=1.14;
+	float sum = a + b;
+	printf("so dau tien: a=%f \n",a);
+	printf("so tiep theo: b=%f \n",b);
+	printf("tong la: sum=%f",sum);//tong bien
+	return 0;//ket thuc chuong trinh
+}
