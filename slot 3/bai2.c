68c99158d941d5869518305fb04f6ce5b4043368
lechieuhoanganh-rgb <lechieuhoanganh@gmail.com>
2026-01-14 09:01:47 +0700

    bai1 va 2

--- slot 3/bai2.c
+++ slot 3/bai2.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
 	char kt = 'a';
-     // Dòng tên
+     // Dòng tên kiểm soát khoảng cách
     printf("%-10s %-10s %-10s %-10s %-10s %-10s\n","ten bien", "gia tri", "dec", "hex", "oct", "size");
-    // Dòng giá trị
+    // Dòng giá trị chuẩn vói khoảng cách đầu dòng
     printf("%-10s %-10c %-10d %-10x %-10o %-10zu\n","kt", kt, kt, kt, kt,sizeof(kt));
 	return 0;
 }
