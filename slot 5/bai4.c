806ee9f5d8f06a0179d6eb11bf8e34e79ed5fa31
lechieuhoanganh-rgb <lechieuhoanganh@gmail.com>
2026-01-21 09:16:33 +0700

    new

--- slot 5/bai4.c
+++ slot 5/bai4.c
@@ -4,25 +4,26 @@ int main(){
 	N=950;
 	printf("nhap t = ");
 	scanf("%d",&t);
-	if (t<100){
-		N = N + 0;
-		printf("So tien dien cua %d kwhs la: %d",t,N);
+	if (t <= 0) {
+    	printf("sai roi");
 	}
-	else if (t>=100 && t<150){
-		N = N + 300;
-		printf("So tien dien cua %d kwhs la: %d",t,N);
+	else if (t < 100) {
+    	N = N + 0;
+    	printf("So tien dien cua %d kwhs la: %d", t, N);
 	}
-	else if (t>=150 && t<200){
-		N = N + 400;
-		printf("So tien dien cua %d kwhs la: %d",t,N);
+	else if (t < 150) {
+    	N = N + 300;
+    	printf("So tien dien cua %d kwhs la: %d", t, N);
 	}
-	else if (t>=200){
-		N = N + 500;
-		printf("So tien dien cua %d kwhs la: %d",t,N);
+	else if (t < 200) {
+    	N = N + 400;
+    	printf("So tien dien cua %d kwhs la: %d", t, N);
 	}
-	else if (t<=0){
-		printf("sai roi");
+	else {
+    	N = N + 500;
+    	printf("So tien dien cua %d kwhs la: %d", t, N);
 	}
+
 	return 0;
 	
 }
